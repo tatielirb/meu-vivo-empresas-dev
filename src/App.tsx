011e7d7fc09e3b2d9c83c23f875963f65f514a59
tatielirb <tatielirb@gmail.com>
2024-02-27 21:57:46 -0300

    feat: aplication titles in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import "App.scss";
 import Header from "components/Header/Index";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-12">
+            <p className="h4">Tecnologias</p>
+          </div>
+          <div className="col-12">
+            <p className="h4">Descobrir</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
